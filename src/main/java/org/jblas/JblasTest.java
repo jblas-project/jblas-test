e520f5541f8d963ef22dbf899d57588c71806944
mikiobraun <mikiobraun@gmail.com>
2020-08-23 16:34:07 +0200

    initial commit

---
+++ src/main/java/org/jblas/JblasTest.java
@@ -0,0 +1,9 @@
+package org.jblas;
+
+public class JblasTest {
+    public static void main(String[] args) {
+        System.out.println("hi");
+
+        org.jblas.benchmark.Main.main(args);
+    }
+}
